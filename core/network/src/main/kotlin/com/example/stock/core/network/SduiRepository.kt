797b2eab8c561fa61880303b51b5d554a22df7d1
gahyunkim <mapledt001@naver.com>
2026-08-18 18:06:20 +0900

    refactor: 기존 루트 경로의 안드로이드 파일 정리 (android/ 이동 마무리) (#4)

--- core/network/src/main/kotlin/com/example/stock/core/network/SduiRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.stock.core.network
-
-import com.example.stock.core.model.UiScreen
-
-/** 서버로부터 SDUI 화면 스키마를 가져오는 창구. 실제 API 연동 전까지는 [FakeSduiRepository]를 쓴다. */
-interface SduiRepository {
-    suspend fun fetchScreen(screenId: String): UiScreen
-}
